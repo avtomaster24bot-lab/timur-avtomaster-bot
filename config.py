3268375243e2d3e90c717db9ac9294d6925cab01
avtomaster24bot-lab <avtomaster24bot@gmail.com>
2026-05-05 14:07:13 +0500

    Change bot token and API key in config.py
    Updated BOT_TOKEN and GEMINI_API_KEY with new values.

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 BOT_LINK = "@AvtoMaster_24_bot"
 
 # --- Telegram ---
-BOT_TOKEN = os.getenv("BOT_TOKEN")            # токен вашего бота
+BOT_TOKEN = os.getenv("@AvtoMaster_24_bot")            # токен вашего бота
 CHANNEL_ID = os.getenv("CHANNEL_ID")          # например, "@avtomaster_channel"
 
 # --- Google Gemini API ---
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")  # ключ для генерации картинок
+GEMINI_API_KEY = os.getenv("AIzaSyDZUrxGKMfOJBe-SM7XvbLzh4yYpPK_xgA")  # ключ для генерации картинок
